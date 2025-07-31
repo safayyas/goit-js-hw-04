75cb764db6ae18ddcda27393af00157061904083
safayyas <sophia.shev@gmail.com>
2025-08-01 00:28:47 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,17 @@
-function slugify(title) {
-  let lowercase = title.toLowerCase();
-  let slug = lowercase.replaceAll(` `, `-`);
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  const totalQuantity = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
+
+  return totalQuantity <= containerSize;
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+// Перевірка функції
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
